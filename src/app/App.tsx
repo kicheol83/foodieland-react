1c08c8f4f964a4cc430c9341825fd7b6b7bdd46e
kicheol83 <suyikim7@gmail.com>
2025-05-18 03:27:10 +0900

    feat: MUI standart integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 function App() {
-  return <div>FOODIELAND</div>;
+  return (
+    <Container sx={{background: "red"}}>
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Foodieland
+          </Typography>
+        </Box>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
-
-
-
-
-
