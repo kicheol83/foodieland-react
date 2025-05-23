649735e6eccf3a3bdee83f607b36de6378f20e81
kicheol83 <suyikim7@gmail.com>
2025-05-24 04:24:24 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import "../css/app.css";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Container } from "@mui/material";
+import { Route, Link, Switch } from "react-router-dom";
+import { About } from "./screens/About";
+import { Users } from "./screens/Users";
 
 function App() {
   return (
-    <Container sx={{ background: "red" }}>
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h4"}>
-            Foodieland
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={5}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
+}
+
+function Home() {
+  return <Container>Home</Container>;
 }
 
 export default App;
