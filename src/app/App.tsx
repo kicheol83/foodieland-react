2de09b4e11bb7e48d51a9a5ee9148144ea05695a
kicheol83 <suyikim7@gmail.com>
2025-05-24 04:00:07 +0900

    feat: MUI customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "../css/app.css";
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
   return (
-    <Container sx={{background: "red"}}>
+    <Container sx={{ background: "red" }}>
       <Stack flexDirection={"column"}>
         <Box sx={{ my: 4 }}>
           <Typography variant="h4" component={"h4"}>
             Foodieland
           </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={5}>
+            <Button variant="contained">Contained</Button>
+          </RippleBadge>
         </Box>
       </Stack>
     </Container>
