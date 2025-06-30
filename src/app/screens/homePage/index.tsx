d1882e33652c4cfd3fa9ad6690951b05eccf990b
kicheol83 <suyikim7@gmail.com>
2025-07-01 01:28:34 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { Container } from "@mui/material";
 import Categories from "./Categories";
 import CheckInstagram from "./CheckInstagram";
@@ -7,7 +8,28 @@ import LeanMore from "./LeanMore";
 import Process from "./Process";
 import Simple from "./Simple";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setRecipeCategories } from "./slice";
+import { retrieveRecipeCategories } from "./selector";
+import { Recipe } from "../../../libs/types/recipe";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setRecipeCategories: (data: Recipe[]) => dispatch(setRecipeCategories(data)),
+});
+const recipeCategoriesRetrieve = createSelector(
+  retrieveRecipeCategories,
+  (recipeCategories) => ({ recipeCategories })
+);
+
 export default function HomePage() {
+  const { setRecipeCategories } = actionDispatch(useDispatch());
+  const { recipeCategories } = useSelector(recipeCategoriesRetrieve);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="home-page">
       <Container>
