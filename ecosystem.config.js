01f7c1cfee0447b425ac4261d45ffd23ff388a90
kicheol83 <suyikim7@gmail.com>
2025-09-02 07:27:09 +0900

    fix: modify ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,22 @@
+const isWindows = process.platform === "win32";
+
 module.exports = {
   apps: [
     {
       name: "FOODIELAND-FRONTEND",
-      script: "serve.cmd",
+      script: isWindows ? "serve.cmd" : "serve",
       args: "-s build -l 3000",
       watch: false,
       interpreter: "none",
       env: {
         NODE_ENV: "production",
       },
-      error_file: "C:/projects/foodieland-react/logs/err.log",
-      out_file: "C:/projects/foodieland-react/logs/out.log",
+      error_file: isWindows
+        ? "C:/projects/foodieland-react/logs/err.log"
+        : "/home/foodieland-project/foodieland-react/logs/err.log",
+      out_file: isWindows
+        ? "C:/projects/foodieland-react/logs/out.log"
+        : "/home/foodieland-project/foodieland-react/logs/out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
